3a85b4cf5ca488b51c7d386468f46b822774e4ce
kamildzi <git-kd@uvi.pl>
2024-11-10 22:58:18 +0100

    Maintenance - minor fixes.

--- eesync.py
+++ eesync.py
@@ -5,10 +5,5 @@ from os import chdir, path
 chdir(path.dirname(path.realpath(__file__)))
 from Src.EESync import EESync # noqa
 
-
-def main():
-    EESync().start()
-
-
 if __name__ == '__main__':
-    main()
+    EESync().start()
